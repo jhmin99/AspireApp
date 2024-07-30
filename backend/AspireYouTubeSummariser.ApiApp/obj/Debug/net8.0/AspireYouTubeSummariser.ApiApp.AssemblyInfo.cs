003130b4537d1654ccdc5a82f44456222288349d
jhmin99 <mjh012399@gmail.com>
2024-07-30 17:35:58 +0900

    Add backend

--- backend/AspireYouTubeSummariser.ApiApp/obj/Debug/net8.0/AspireYouTubeSummariser.ApiApp.AssemblyInfo.cs
+++ backend/AspireYouTubeSummariser.ApiApp/obj/Debug/net8.0/AspireYouTubeSummariser.ApiApp.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("AspireYouTubeSummariser.WebApp")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("AspireYouTubeSummariser.ApiApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bf1f39f3c165f6d790a0f6d23ac6271b77f206f")]
-[assembly: System.Reflection.AssemblyProductAttribute("AspireYouTubeSummariser.WebApp")]
-[assembly: System.Reflection.AssemblyTitleAttribute("AspireYouTubeSummariser.WebApp")]
+[assembly: System.Reflection.AssemblyProductAttribute("AspireYouTubeSummariser.ApiApp")]
+[assembly: System.Reflection.AssemblyTitleAttribute("AspireYouTubeSummariser.ApiApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment 클래스에서 생성되었습니다.
--- frontend/AspireYouTubeSummariser.WebApp/Clients/ApiAppClient.cs
+++ frontend/AspireYouTubeSummariser.WebApp/Clients/ApiAppClient.cs
@@ -36,3 +36,4 @@ public class ApiAppClient(HttpClient http) : IApiAppClient
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
     }
 }
+
