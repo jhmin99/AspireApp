20e6ff4ab1d5791850266fcde46387eb2950f23e
jhmin99 <mjh012399@gmail.com>
2024-07-30 18:48:03 +0900

    Add GitHub Actions workflow for Azure deployment

--- Aspire/AspireYouTubeSummariser.AppHost/Program.cs
+++ Aspire/AspireYouTubeSummariser.AppHost/Program.cs
@@ -1,12 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 var config = builder.Configuration;
-var apiapp = builder.AddProject<Projects.AspireYouTubeSummariser_ApiApp>("apiapp")
+var apiapp = builder.AddProject<Projects.AspireYouTubeSummariser_ApiApp>("backend/AspireYouTubeSummariser.ApiApp")
                     .WithEnvironment("OpenAI__Endpoint", config["OpenAI:Endpoint"])
                     .WithEnvironment("OpenAI__ApiKey", config["OpenAI:ApiKey"])
                     .WithEnvironment("OpenAI__DeploymentName", config["OpenAI:DeploymentName"]);
 
 
-builder.AddProject<Projects.AspireYouTubeSummariser_WebApp>("webapp")
+builder.AddProject<Projects.AspireYouTubeSummariser_WebApp>("frontend/AspireYouTubeSummariser.WebApp")
     .WithExternalHttpEndpoints()
     .WithReference(apiapp);
 builder.Build().Run();
